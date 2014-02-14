b4e63e884f79d71e1c1b0d1a0d850bace2c434eb
menaerick <menaerick@hotmail.com>
2014-02-14 12:57:55 -0600

    mas cambios

--- js/acciones.js
+++ js/acciones.js
@@ -1,3 +1,3 @@
 // JavaScript Document
 $(document).ready(function(){document.addEventListener("deviceready",function(){$('#B1').tap(function(){navigator.notificacion.beep(1);}/*tap*/ );})/*device ready*/});/*ready*/
-function(){document.addEventListener("deviceready",function(){$('#V1').tap(function(){navigator.notificacion.vibrate(500);}/*tap*/ );})/*device ready*/};/*ready*/
+/*ready*/
